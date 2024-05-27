f57e10ce60eb782ee9e4ed4d379484fc93ca9053
Getting Flashy <shamsnisar05@gmail.com>
2024-05-27 07:37:54 +0500

    initial commit

---
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -0,0 +1,134 @@
+package com.example.notesapp
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.notesapp.databinding.ActivityMainBinding
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: NoteAdapter
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPreferences: SharedPreferences
+
+    companion object {
+        private const val FILEPATH = "notes.json"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.fab.setOnClickListener { view ->
+            NewNote().show(supportFragmentManager, null)
+        }
+
+        adapter = NoteAdapter(this)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.itemAnimator = DefaultItemAnimator()
+
+        // Launching the retrieveNotes function whenever the app is launched
+        // to display the saved notes
+        adapter.noteList = retrieveNotes()
+        // notifyItemRangeInserted will tell the recyclerview that one or more items have been added to the RecyclerView
+        adapter.notifyItemRangeInserted(0, adapter.noteList.size)
+
+        // Initialized the shared preferences variable that is defined at top as a lateinit variable
+        // to save and access the shared preferences file
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val nightThemeSelected = sharedPreferences.getBoolean("theme", false)
+        if (nightThemeSelected) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        val showDividingLines = sharedPreferences.getBoolean("dividingLines", false)
+        if (showDividingLines) binding.recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+        else if (binding.recyclerView.itemDecorationCount > 0) binding.recyclerView.removeItemDecorationAt(0)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    fun createNewNote(note: Note) {
+        adapter.noteList.add(note)
+        adapter.notifyItemInserted(adapter.noteList.size -1)
+        saveNotes()
+    }
+
+    fun deleteNote(index: Int) {
+        adapter.noteList.removeAt(index)
+        adapter.notifyItemRemoved(index)
+        saveNotes()
+    }
+
+    fun showNote(index: Int) {
+        val dialog = ShowNote(adapter.noteList[index], index)
+        dialog.show(supportFragmentManager, null)
+    }
+
+    fun saveNotes() {
+        val notes = adapter.noteList
+        val gson = GsonBuilder().create()
+        val jsonNotes = gson.toJson(notes)
+
+        val outputStream = openFileOutput(FILEPATH, Context.MODE_PRIVATE)
+        OutputStreamWriter(outputStream).use { writer ->
+            writer.write(jsonNotes)
+        }
+    }
+
+    private fun retrieveNotes(): MutableList<Note> {
+        val noteList = mutableListOf<Note>()
+        if (getFileStreamPath(FILEPATH).isFile) {
+            val fileInput = openFileInput(FILEPATH)
+            BufferedReader(InputStreamReader(fileInput)).use { reader ->
+                val stringBuilder = StringBuilder()
+                for (line in reader.readLine()) stringBuilder.append(line)
+
+                if (stringBuilder.isNotEmpty()){
+                    val listType = object : TypeToken<List<Note>>() {}.type
+                    noteList.addAll(Gson().fromJson(stringBuilder.toString(), listType))
+                }
+            }
+        }
+        return noteList
+    }
+
+}
---
+++ app/src/main/java/com/example/notesapp/NewNote.kt
@@ -0,0 +1,49 @@
+package com.example.notesapp
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.example.notesapp.databinding.NewNoteBinding
+
+class NewNote : DialogFragment() {
+
+    private var _binding: NewNoteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val mainActivity = activity as MainActivity
+        val inflater = mainActivity.layoutInflater
+        _binding = NewNoteBinding.inflate(inflater)
+
+        val builder = AlertDialog.Builder(mainActivity)
+            .setView(binding.root)
+            .setMessage(resources.getString(R.string.add_new_note))
+
+        binding.btnCancel.setOnClickListener {
+            dismiss()
+        }
+
+        binding.btnOK.setOnClickListener {
+            val title = binding.editTitle.text.toString()
+            val contents = binding.editContents.text.toString()
+
+            if (title.isNotEmpty() && contents.isNotEmpty()) {
+                val note = Note(title, contents)
+                mainActivity.createNewNote(note)
+
+                Toast.makeText(mainActivity, resources.getString(R.string.note_saved), Toast.LENGTH_SHORT).show()
+
+                dismiss()
+            } else Toast.makeText(mainActivity, resources.getString(R.string.note_empty), Toast.LENGTH_LONG).show()
+        }
+        return builder.create()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
